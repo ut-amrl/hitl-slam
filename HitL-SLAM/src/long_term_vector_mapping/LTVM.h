55b3b613b790d78b3ad30de44aeca525e2ba4035
Samer Nashed <samernashed@gmail.com>
2018-01-27 10:54:18 -0500

    fixed erroneous request for map_libraries in gui CMakeLists leading to linker errors

--- HitL-SLAM/src/long_term_vector_mapping/LTVM.h
+++ HitL-SLAM/src/long_term_vector_mapping/LTVM.h
@@ -1,6 +1,11 @@
 class LongTermVectorMap {
   public:
-
+    void init();
+    void Curate(std::vector<perception_2d::Pose2Df> poses,
+                std::vector<perception_2d::PointCloudf> point_clouds,
+                Eigen::Affine2f map_transform);
   private:
-
+    SDFFilter();
+    
+    
 };
